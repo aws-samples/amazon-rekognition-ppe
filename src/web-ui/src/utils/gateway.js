897d341c252d5863c78ddea0694b97f82a0a6868
Connor Kirkpatrick <ckp@amazon.com>
2020-06-02 17:23:25 +0100

    Update src/web-ui/src/utils/gateway.js
    Co-authored-by: Matteo Figus <matteofigus@gmail.com>

--- src/web-ui/src/utils/gateway.js
+++ src/web-ui/src/utils/gateway.js
@@ -1,12 +1,6 @@
 import request from "./request";
 
 export default {
-  addUser(params) {
-    return request("/faces/index", "post", {
-      image: params.image,
-      fullName: params.fullName,
-    });
-  },
 
   processImage(image) {
     return request("/process", "post", { image });
