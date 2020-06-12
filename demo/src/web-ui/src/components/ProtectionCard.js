4bdcbb33c056ad34d532df67607bb2e5871e9970
Matteo Figus <matteofigus@gmail.com>
2020-06-12 15:02:55 +0100

    Showing confidence and more details about body parts and equipment

--- demo/src/web-ui/src/components/ProtectionCard.js
+++ demo/src/web-ui/src/components/ProtectionCard.js
@@ -3,40 +3,42 @@ import { Card, ListGroup } from "react-bootstrap";
 
 import Icon from "./Icon";
 
-const TestResult = ({ testResult }) => (
-  <ListGroup.Item>
-    <Icon type={testResult.Success ? "success" : "fail"} />
-    {testResult.TestName}
-  </ListGroup.Item>
+export default ({ person, webcamCoordinates }) => (
+  <Card style={{ marginTop: "20px", textAlign: "left" }}>
+    <Card.Header>{`Person #${person.id}`}</Card.Header>
+    <div
+      key={`bb-${person.id}`}
+      className={`bb-${person.id} bounding-box`}
+      style={{
+        height: webcamCoordinates.height * person.boundingBox.Height,
+        left:
+          webcamCoordinates.left +
+          person.boundingBox.Left * webcamCoordinates.width,
+        top:
+          webcamCoordinates.top +
+          person.boundingBox.Top * webcamCoordinates.height,
+        width: webcamCoordinates.width * person.boundingBox.Width,
+      }}
+    >
+      <span className="box-label">Person #{person.id}</span>
+    </div>
+    <Card.Body>
+      {person.results.map((r, index) => (
+        <ListGroup key={index} className="detection-part">
+          <ListGroup.Item>
+            {r.type} detected
+            <span className="confidence">{r.confidence}%</span>
+          </ListGroup.Item>
+          <ListGroup.Item key={index}>
+            {r.type} covers {r.bodyPart}:{" "}
+            <Icon type={r.coversBodyPart ? "success" : "fail"} />{" "}
+            <span style={{ color: r.coversBodyPart ? "#1d8102" : "#d13212" }}>
+              {r.coversBodyPart.toString()}
+            </span>
+            <span className="confidence">{r.coversBodyPartConfidence}%</span>
+          </ListGroup.Item>
+        </ListGroup>
+      ))}
+    </Card.Body>
+  </Card>
 );
-
-export default ({ person, webcamCoordinates }) => {
-  const result = person.results.map((test, index) => (
-    <TestResult testResult={test} key={index}></TestResult>
-  ));
-
-  return (
-    <Card style={{ marginTop: "20px", textAlign: "left" }}>
-      <Card.Header>{`Person #${person.id}`}</Card.Header>
-      <div
-        key={`bb-${person.id}`}
-        className={`bb-${person.id} bounding-box`}
-        style={{
-          height: webcamCoordinates.height * person.boundingBox.Height,
-          left:
-            webcamCoordinates.left +
-            person.boundingBox.Left * webcamCoordinates.width,
-          top:
-            webcamCoordinates.top +
-            person.boundingBox.Top * webcamCoordinates.height,
-          width: webcamCoordinates.width * person.boundingBox.Width,
-        }}
-      >
-        <span className="box-label">Person #{person.id}</span>
-      </div>
-      <Card.Body>
-        <ListGroup>{result}</ListGroup>
-      </Card.Body>
-    </Card>
-  );
-};
